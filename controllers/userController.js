fa6b4945ae84b91685ca5b1a934cfa0c0146f1ff
Radhika <cu9rad@gmail.com>
2022-04-10 01:30:28 -0400

    modified controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const { User } = require('../models');
 
 const userController = {
+    //get all users
     getUsers(req, res) {
         User.find({})
             .then(userData => res.json(userData))
@@ -9,20 +10,26 @@ const userController = {
                 res.status(400).json(err);
             });
     },
+
+    //create user
     addUser({ body }, res) {
         User.create(body)
             .then(userData => res.json(userData))
             .catch(err => res.status(400).json(err));
     },
+
+    //get single user by id
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
             .then(userData => res.json(userData))
             .catch(err => res.status(400).json(err));
     },
+
+    //update user by id
     updateUser({ params, body }, res) {
         User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
             .then(userData => {
-                if(!userData) {
+                if (!userData) {
                     res.status(404).json({ message: 'No user found with this ID!' });
                     return;
                 }
@@ -31,39 +38,41 @@ const userController = {
             })
             .catch(err => res.status(400).json(err));
     },
-    deleteUser({params}, res) {
-        User.findOneAndDelete({_id: params.id})
-        .then(userData => {
-            if(!userData) {
-                res.status(404).json({ message: 'No user found with this ID!' });
-                return;
-            }
 
-            res.json(userData);
-        })
-        .catch(err => res.status(400).json(err));
+    //delete user and his thoughts
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(userData => !userData ?
+                res.status(404).json({ message: 'No user found with this ID!' }) :
+                User.deleteMany({ _id: { $in: User.thoughts } }))
+
+            .then(() => res.json({ message: 'User and thoughts deleted!' }))
+            .catch((err) => res.status(500).json(err))
     },
-    addFriend({params}, res){
+
+
+    //add a friend
+    addFriend({ params }, res) {
         User.findOneAndUpdate(
-            {_id: params.id},
-            {$push: {friends: params.friendId}},
-            {runValidators: true, new: true}
+            { _id: params.id },
+            { $push: { friends: params.friendId } },
+            { runValidators: true, new: true }
         )
-        .then(userData => {
-            if(!userData) {
-                res.status(404).json({ message: 'No user found with this ID!' });
-                return;
-            }
-            res.json(userData);
-        })
-        .catch(err => res.status(400).json(err));
+            .then(userData => {
+                if (!userData) {
+                    res.status(404).json({ message: 'No user found with this ID!' });
+                    return;
+                }
+                res.json(userData);
+            })
+            .catch(err => res.status(400).json(err));
     },
     //delete friend
- 
-    deleteFriend({ params}, res) {
+
+    deleteFriend({ params }, res) {
         User.findOneAndDelete({
-                _id: params.id
-            })
+            _id: params.id
+        })
             .then(deletedFriend => {
                 if (!deletedFriend) {
                     return res.status(404).json({
@@ -71,9 +80,10 @@ const userController = {
                     })
                 }
                 return User.findOneAndUpdate({
-                    friends: params.friendId },
-                     { $pull: { friends: params.friendId }},
-                     { new: true });
+                    friends: params.friendId
+                },
+                    { $pull: { friends: params.friendId } },
+                    { new: true });
             })
             .then(dbUserData => {
                 if (!dbUserData) {
@@ -87,5 +97,5 @@ const userController = {
             .catch(err => res.json(err));
     },
 };
-    
+
 module.exports = userController;
